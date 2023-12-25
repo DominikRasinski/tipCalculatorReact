5894e59a8e183d144b4a5130e1f9b547fed9b11a
Dominik Rasiński <dox.gmr@gmail.com>
2023-12-25 23:02:44 +0100

    add some logic and some css for buttons

--- src/componnents/tipCalculator/tipCalculator.tsx
+++ src/componnents/tipCalculator/tipCalculator.tsx
@@ -18,10 +18,11 @@ export const MainCalculator = (props: calculatorProps) => {
 
 const LeftCalculatorStructure = (props: calculatorProps) => {
 
-    const [tip, setTip] = React.useState(0);
-    const [bill, setBill] = React.useState(props?.value ?? '');
-    const [people, setPeople] = React.useState(props?.value ?? '');
-    const [input, setInput] = React.useState(props?.value ?? '');
+    let total = 0;
+    let [tip, setTip] = React.useState(0);
+    const [bill, setBill] = React.useState(0);
+    const [people, setPeople] = React.useState(0);
+    const [input, setInput] = React.useState(0);
 
     const handleInput = (event: any) => {
         setInput(event.target.value)
@@ -33,24 +34,33 @@ const LeftCalculatorStructure = (props: calculatorProps) => {
         setPeople(event.targer.value)
     }
 
+    if(input !== 0) {
+        tip = input;
+    }
+
+    if (tip !== 0 && bill !== 0 && people !== 0) {
+        let sum = (bill * tip / 100);
+        total = sum / people;
+    }
+
     console.log(bill);
     console.log(people);
 
     return (
         <div className={`${css.leftContainer}`}>
             <p>Bill</p>
-            <input type="text" name="" id="" value={bill} onInput={handleBill} />
+            <input className={`${css.inputBase}`} type="text" name="" id="" value={bill} onInput={handleBill} />
             <p>Select Tip %</p>
             <div className='flex flex-row justify-between flex-wrap'>
-                <button className={`${css.buttons}`} onClick={() => setTip(5)}>5%</button>
-                <button className={`${css.buttons}`} onClick={() => setTip(10)}>10%</button>
-                <button className={`${css.buttons}`} onClick={() => setTip(15)}>15%</button>
-                <button className={`${css.buttons}`} onClick={() => setTip(25)}>25%</button>
-                <button className={`${css.buttons}`} onClick={() => setTip(50)}>50%</button>
-                <input type="text" name="" id="" className={`${css.buttons}`} value={input} onInput={handleInput}/>
+                <button className={`${css.buttons} ${css.buttonsBase}`} onClick={() => setTip(5)}>5%</button>
+                <button className={`${css.buttons} ${css.buttonsBase}`} onClick={() => setTip(10)}>10%</button>
+                <button className={`${css.buttons} ${css.buttonsBase}`} onClick={() => setTip(15)}>15%</button>
+                <button className={`${css.buttons} ${css.buttonsBase}`} onClick={() => setTip(25)}>25%</button>
+                <button className={`${css.buttons} ${css.buttonsBase}`} onClick={() => setTip(50)}>50%</button>
+                <input type="text" name="" id="" className={`${css.btnCustom} ${css.buttonsBase}`} value={input} onInput={handleInput}/>
             </div>
             <p>Number of People</p>
-            <input type="text" name="" id="" value={people} onInput={handlePeople} />
+            <input className={`${css.inputBase}`} type="text" name="" id="" value={people} onInput={handlePeople} />
         </div>
     )
 }
@@ -65,6 +75,9 @@ const RightCalculatorStructure = (props: calculatorProps) => {
             <div>
                 <p className='inline-block w-1/3'>Total</p><span>/ person</span>
                 <p className='inline-block w-1/2'><span>$</span>0.00</p>
+            </div>
+            <div className='flex justify-center align-middle w-full'>
+                <button className={`${css.btnReset}`} type="reset">reset</button>
             </div>
         </div>
     )
